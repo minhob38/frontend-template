63b4935166c023f7c1d9730592bc5f5ca430ef5c
minho <minhob38@gmail.com>
2023-11-10 15:18:32 +0900

    feat: Add socket connection

--- react-template/src/components/Plane.tsx
+++ react-template/src/components/Plane.tsx
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
-import React from "react";
+import React, { useEffect } from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import { io } from "socket.io-client";
 import kfx from "../assets/images/kfx.jpeg";
 
 /*
@@ -13,11 +14,34 @@ const CSS = css`
 `;
 const Img = styled.img``;
 
+const SOCKET_SERVER_ADDRESS = "http://localhost:8000";
+const SOCKET_SERVER_NAMESPACE = "dashboard/event";
+const socket = io(`${SOCKET_SERVER_ADDRESS}/${SOCKET_SERVER_NAMESPACE}`, {
+  transports: ["websocket"],
+});
+socket.connect();
+const PUBLISH_EVENT = "subscribeChatEvent";
+const SUBSCRIBE_EVENT = "dashboardChatDeleteEvent";
+
+const MESSAGE = { userId: "63", roomType: "dashboard" };
 const Plane: React.FC = () => {
+  useEffect(() => {
+    socket.on(SUBSCRIBE_EVENT, (data) => {
+      console.log(`subscribe: ${SUBSCRIBE_EVENT}`);
+      console.log(data);
+    });
+  }, []);
+
+  const handleClick = () => {
+    console.log(`publish: ${PUBLISH_EVENT}`);
+    socket.emit(PUBLISH_EVENT, MESSAGE, (response) => console.log(response));
+  };
+
   return (
     <>
       <div css={CSS}>Plane 🛩</div>
       <Img src={kfx} alt="kfx" width={150} />
+      <button onClick={handleClick}>Publish</button>
     </>
   );
 };
